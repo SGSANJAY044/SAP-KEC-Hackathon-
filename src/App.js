cab36e275f73a837e6b47db766d01c1a51566e26
SGSANJAY044 <sgsanjay044@gmail.com>
2023-02-05 09:30:06 +0530

    Tailwind config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css';
-
+import Home from './Pages/Login/Login.jsx'
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
